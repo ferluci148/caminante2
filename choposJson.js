77b8ca2bc13011ce33b00a9c35195a57c4ebb89e
ferluci148 <84928726+ferluci148@users.noreply.github.com>
2024-05-27 19:52:00 +0200

    Create choposJson.js

---
+++ choposJson.js
@@ -0,0 +1,138 @@
+ function ControlFincas($scope) {   
+            $scope.fincas = [
+                {
+                    "id": "1",
+                    "descripcion": "La Presa",
+                    "poligono":"529",
+                    "parcela":"910",
+                    "plantacion": "Chopos",
+                    "superficie": "45a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.69813473375674",
+                    "longitud":" -3.5735607147216797",
+                    "recinto":"new google.maps.LatLng(41.69813473375674, -3.5735607147216797),new google.maps.LatLng(41.69781430131848, -3.572101593017578),new google.maps.LatLng(41.69739773676192, -3.572230339050293),new google.maps.LatLng(41.69781430131848, -3.573904037475586),new google.maps.LatLng(41.69813473375674, -3.5735607147216797)"
+                   
+                },
+                {
+    
+ 
+                    "id": "2",  
+                    "descripcion": "Puente de la Tajo",
+                     "poligono":"529",
+                    "parcela":"878",
+                    "plantacion": "Chopos",
+                    "superficie": "71a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.6937767158053",
+                    "longitud":"-3.575577735900879",
+                    "recinto":" new google.maps.LatLng( 41.6937767158053, -3.575577735900879),new google.maps.LatLng(41.693904897018726, -3.5736465454101562),new google.maps.LatLng(41.69350432987867, -3.5734963417053222),new google.maps.LatLng(41.693328079546774, -3.5755348205566406),new google.maps.LatLng(41.6937767158053, -3.575577735900879)"
+                },
+                {
+                    "id":"3",
+                    "descripcion": "Zazuar",
+                     "poligono":"512",
+                    "parcela":"341",
+                    "plantacion": "Chopos",
+                    "superficie": "88a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.700890386820035",
+                    "longitud":" -3.5673165321350098",
+                    "recinto":"new google.maps.LatLng(41.700890386820035, -3.5673165321350098),new google.maps.LatLng(41.70084232411562, -3.5641837120056152),new google.maps.LatLng(41.70114672063683, -3.564162254333496),new google.maps.LatLng(41.70138703266202, -3.5675954818725586),new google.maps.LatLng(41.700890386820035, -3.5673165321350098)"
+                } ,
+                {
+                    "id":"4",
+                    "descripcion": "Soto Bajero",
+                     "poligono":"531",
+                    "parcela":"1059",
+                    "plantacion": "Chopos",
+                    "superficie": "95a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.69005934949943",
+                    "longitud":"-3.5948359966278076",
+                    "recinto":"new google.maps.LatLng(41.69005934949943, -3.5948359966278076),new google.maps.LatLng(41.69016350277359, -3.593752384185791),new google.maps.LatLng(41.68881750900814, -3.593430519104004),new google.maps.LatLng(41.6887854611943, -3.5946428775787354),new google.maps.LatLng(41.69005934949943, -3.5948359966278076)"
+                } ,
+                {
+                    "id":"5",
+                    "descripcion": "LO Malo 1",
+                     "poligono":"532",
+                    "parcela":"1102",
+                    "plantacion": "Chopos",
+                    "superficie": "39a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.69039584408525",
+                    "longitud":"-3.602914810180664",
+                    "recinto":"new google.maps.LatLng(41.69039584408525, -3.602914810180664),new google.maps.LatLng(41.690532043774375, -3.6022067070007324),new google.maps.LatLng(41.689033831334065, -3.602238893508911),new google.maps.LatLng(41.68906587902419, -3.6027538776397705),new google.maps.LatLng(41.69039584408525, -3.602914810180664)"
+                } ,
+                {
+                    "id":"6",
+                    "descripcion": "Lo Malo Pilar",
+                     "poligono":"532",
+                    "parcela":"1090",
+                    "plantacion": "Chopos",
+                    "superficie": "76a",
+                    "cepas":"--",
+                    "edad":"-",
+                    "latitud":"41.68975490050034",
+                    "longitud":"-3.6064231395721436",
+                    "recinto":"new google.maps.LatLng(41.68975490050034, -3.6064231395721436),new google.maps.LatLng(41.68986706608868, -3.6058545112609863),new google.maps.LatLng(41.68855311406642, -3.605661392211914),new google.maps.LatLng(41.68854510208153, -3.606240749359131),new google.maps.LatLng(41.68975490050034, -3.6064231395721436)"
+                }  ,
+                {
+                    "id":"7",
+                    "descripcion": "Lo Malo Aytmto.",
+                     "poligono":"532",
+                    "parcela":"1088",
+                    "plantacion": "Chopos",
+                    "superficie": "20a",
+                    "cepas":"600",
+                    "edad":"30/12",
+                    "latitud":"41.6896988176328",
+                    "longitud":"-3.606905937194824",
+                    "recinto":"new google.maps.LatLng(41.6896988176328, -3.606905937194824),new google.maps.LatLng(41.68970682947404, -3.6067771911621094),new google.maps.LatLng(41.688569138033216, -3.6066484451293945),new google.maps.LatLng(41.68853709009564, -3.6067771911621094),new google.maps.LatLng(41.6896988176328, -3.606905937194824)"
+                } ,
+                {
+                    "id":"8",
+                    "descripcion": "Lo Malo J.Antonio",
+                     "poligono":"532",
+                    "parcela":"1105/1104",
+                    "plantacion": "Chopos",
+                    "superficie": "1h 85 a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.690924617735384",
+                    "longitud":" -3.5998141765594482",
+                    "recinto":"new google.maps.LatLng(41.690924617735384, -3.5998141765594482),new google.maps.LatLng(41.69113292131326, -3.5989129543304443),new google.maps.LatLng(41.688881604587955, -3.5989880561828613),new google.maps.LatLng(41.68896172397291, -3.599867820739746),new google.maps.LatLng(41.690924617735384, -3.5998141765594482)"
+                } ,
+                {
+                    "id":"9",
+                    "descripcion": "Cayetano",
+                     "poligono":"--",
+                    "parcela":"--",
+                    "plantacion": "Chopos",
+                    "superficie": "95a",
+                    "cepas":"--",
+                    "edad":"-",
+                    "latitud":"41.69081245399098",
+                    "longitud":"-3.583195209503174",
+                    "recinto":"new google.maps.LatLng(41.69081245399098, -3.583195209503174),new google.maps.LatLng(41.69090859435529, -3.5825729370117188),new google.maps.LatLng(41.688985759768954, -3.582444190979004),new google.maps.LatLng(41.68880949705617, -3.5830235481262207),new google.maps.LatLng(41.69081245399098, -3.583195209503174)"
+                } ,
+                {
+                    "id":"10",
+                    "descripcion": "Bajamontero",
+                     "poligono":"503",
+                    "parcela":"18",
+                    "plantacion": "Chopos",
+                    "superficie": "1h 33a",
+                    "cepas":"--",
+                    "edad":"--",
+                    "latitud":"41.72084932414073",
+                    "longitud":"-3.5651063919067383",
+                    "recinto":"new google.maps.LatLng(41.72084932414073, -3.5651063919067383),new google.maps.LatLng(41.72064111681767, -3.5633039474487305),new google.maps.LatLng(41.719728199822235, -3.5637545585632324),new google.maps.LatLng(41.719680151200066, -3.564140796661377),new google.maps.LatLng(41.71960007008327, -3.564333915710449),new google.maps.LatLng(41.720064539171524, -3.565213680267334),new google.maps.LatLng(41.72084932414073, -3.5651063919067383)"
+                }  
+            
+  ];
+         };
