b35d5abd59be555be83010f9521bb65c485673c8
ferluci148 <84928726+ferluci148@users.noreply.github.com>
2024-10-11 21:17:34 +0200

    Add files via upload

---
+++ Cuenca.js
@@ -0,0 +1,31 @@
+const waypoints = [
+    { lat: 39.98871149056718, lng: -2.113934355658098 ,dir:'C. San Isidro, 1, 16123 Arcas, Cuenca, España'},// Arcas de Villar
+    { lat: 39.969351661412084, lng: -1.901336672693148,dir:'C. Real, 27, 16340 Cañada del Hoyo, Cuenca, España' },//Cañada del Hoyo, C
+    { lat: 40.042685840879784, lng: -1.6483038205782807,dir:'C. Boleo, 15, 16300 Cañete, Cuenca, España' },//Cañete, Cuenca
+    { lat: 39.89658549360223, lng: -1.8084982978243769,dir:'Ctra. Camporrobles, 9, 16350 Carboneras de Guadazaón, Cuenca, España' },// Carboneras de Gua
+    { lat: 39.715959, lng: -2.8348038917422347,dir:'C. Alarcon, 16212, Cuenca, España' },//Hontanaya
+    { lat: 39.50153302154944, lng: -2.871527747561935,dir:'C. Tercia, 13, 16630 Mota del Cuervo, Cuenca, España' },//Mota del Cuervo
+    { lat: 39.4790133, lng: -2.0369694843325625,dir:'Pl. del Pilar, 1, 16421 Hontanaya, Cuenca, España' },//Pozorrubielos de la Mancha
+    { lat:39.98865805974147 , lng: -2.114265105557247 ,dir:'Pl. Mayor, 1, 16123 Arcas, Cuenca, España ' }, 
+    { lat: 39.969972612112784, lng: -1.9007652723044433 ,dir:'C. Rosina, 3, 16340 Cañada del Hoyo, Cuenca, España ' }, 
+    { lat:40.01030891973805 , lng: -3.007622888360606 ,dir:'Av. Miguel de Cervantes, 19, 16400 Tarancón, Cuenca, España ' }, 
+    { lat: 39.49645095461005, lng: -2.165221926983645 ,dir:'C. San Miguel, 12, 16710 Tébar, Cuenca, España ' }, 
+    { lat: 39.32722582008963, lng: -1.847396365081786 ,dir:'C. Matías Cuesta, 1, 16236 Villagarcía del Llano, Cuenca, España ' }, 
+    { lat: 39.732387148587875, lng: -2.925719434393317 ,dir:'C. de Gustavo Torner, 11, 16415 Villamayor de Santiago, Cuenca, España' }, 
+    { lat:40.0147240889032 , lng: -2.195896111639404 ,dir:'Tr.ª Pública 2, 2, 16196 Villar de Olalla, Cuenca, España ' }, 
+    { lat:39.8670601297066 , lng: -1.6559295968200716 ,dir:'V88V+J9 Villar del Humo, España ' }, 
+    { lat:40.018744535988525 , lng: -3.1309077349182135 ,dir:'C. Salitreria, 14, 16470 Zarza de Tajo, Cuenca, España ' }, 
+    { lat:39.44060420068243 , lng: -1.749482334393313 ,dir:'C. Ledaña, 2, 16235 Iniesta, Cuenca, España ' }, 
+    { lat:39.66151977558414 , lng: -2.759550334393306 ,dir:'C. Veracruz, 9, 16423 Osa de la Vega, Cuenca, España' }, 
+    { lat:39.615403343370055 , lng: -2.4262368809509205 ,dir:'C. Melgarejo, 18, 16622 Pinarejo, Cuenca, España ' }, 
+    { lat:39.5581812264352 , lng: -2.422342534393309 ,dir:'C. Rubio, 3, 16621 Santa María del Campo Rus, Cuenca, España ' }, 
+    { lat:39.700463381182836 , lng: -2.7546165497375408 ,dir:'C. Hiedra, 5T, 16422 Tresjuncos, Cuenca, España ' }, 
+    { lat:40.01027604963299 , lng: -3.007322480950938 ,dir:'P.º Estación, 5, 16400 Tarancón, Cuenca, España ' }, 
+    { lat:39.40728756547664 , lng: -2.202871288360597 ,dir:'C. Mayor, 17, 16700 Sisante, Cuenca, España ' }, 
+    { lat:39.3606890508175 , lng: -2.1283044269836338 ,dir:'C. Mayor, 1, 16707 Casas de Benítez, Cuenca, España ' }, 
+    { lat:39.56237449149715 , lng: -1.9112523883606003 ,dir:'Calle Dr. de Vicente Vega, 11, 16200 Motilla del Palancar, Cuenca, España ' }, 
+   
+    
+    
+    
+  ]
